b707c36e07a3600b618dbcd8683fa8c6b45a868d
furusho <furusityofu@gmail.com>
2019-05-12 17:38:55 +0900

    add unity files

---
+++ Assets/Out.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Out : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "player")
+        {
+            SceneManager.LoadScene(
+            SceneManager.GetActiveScene().name);
+        }
+    }
+}
